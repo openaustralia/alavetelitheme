30af67e3221e84e92a294f082fda232a9b4c7a19
maxious <maxious@lambdacomplex.org>
2012-01-14 22:10:34 +1100

    Use categories override from asktheeu-theme

---
+++ lib/lib/public_body_categories_en.rb
@@ -0,0 +1,28 @@
+# lib/public_body_categories.rb:
+# Categorisations of public bodies.
+#
+# Copyright (c) 2012 Alexander Sadleir. All rights reserved.
+
+PublicBodyCategories.add(:en, [
+    "Portfolios",
+        [ "attorney-general","Attorney-General","Attorney-General" ],
+        [ "agriculture_fisheries_and_forestry","Agriculture, Fisheries and Forestry","Agriculture, Fisheries and Forestry" ],
+        [ "broadband_communications_and_the_digital_economy","Broadband, Communications and the Digital Economy","Broadband, Communications and the Digital Economy" ],
+        [ "climate_change_and_energy_efficiency","Climate Change and Energy Efficiency","Climate Change and Energy Efficiency" ],
+        [ "defence","Defence","Defence" ],
+        [ "education_employment_and_workplace_relations","Education, Employment and Workplace Relations","Education, Employment and Workplace Relations" ],
+        [ "families_housing_community_services_and_indigenous_affairs","Families, Housing, Community Services and Indigenous Affairs","Families, Housing, Community Services and Indigenous Affairs" ],
+        [ "finance_and_deregulation","Finance and Deregulation","Finance and Deregulation" ],
+        [ "foreign_affairs_and_trade","Foreign Affairs and Trade","Foreign Affairs and Trade" ],
+        [ "health_and_ageing","Health and Ageing","Health and Ageing" ],
+        [ "human_services","Human Services","Human Services" ],
+        [ "immigration_&_citizenship","Immigration & Citizenship","Immigration & Citizenship" ],
+        [ "industry_innovation_science_research_and_tertiary_education","Industry, Innovation, Science, Research and Tertiary Education","Industry, Innovation, Science, Research and Tertiary Education" ],
+        [ "infrastructure_and_transport","Infrastructure and Transport","Infrastructure and Transport" ],
+        [ "regional_australia_regional_development_and_local_government","Regional Australia, Regional Development and Local Government","Regional Australia, Regional Development and Local Government" ],
+        [ "resources_energy_and_tourism","Resources Energy and Tourism","Resources Energy and Tourism" ],
+        [ "sustainability_environment_water_population_and_communities","Sustainability, Environment, Water, Population and Communities","Sustainability, Environment, Water, Population and Communities" ],
+        [ "prime_minister_and_cabinet","Prime Minister and Cabinet","Prime Minister and Cabinet" ],
+        [ "treasury","Treasury","Treasury" ],
+        [ "veterans_affairs","Veterans' Affairs","Veterans' Affairs" ],
+])
