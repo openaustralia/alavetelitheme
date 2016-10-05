026140c532e7dcb914955809881eb68829d1c22b
Henare Degan <henare.degan@gmail.com>
2016-10-05 16:48:42 +1100

    Fetch requester and commenter data from database expose an API

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -12,7 +12,30 @@ Rails.configuration.to_prepare do
     end
 
     def people
+      @all_time_requesters = InfoRequest.visible.joins(:user).group(:user).order("count_all DESC").limit(10).count
+      @last_28_day_requesters = InfoRequest.visible.where("info_requests.created_at >= ?", 28.days.ago).joins(:user).group(:user).order("count_all DESC").limit(10).count
+      # TODO: Only do visible comments
+      @all_time_commenters = Comment.joins(:user).group(:user).order("count_all DESC").limit(10).count
+      # TODO: Only do visible comments
+      @last_28_day_commenters = Comment.where("comments.created_at >= ?", 28.days.ago).joins(:user).group(:user).order("count_all DESC").limit(10).count
 
+      api_data = {
+        all_time_requesters: json_for_api(@all_time_requesters),
+        last_28_day_requesters: json_for_api(@last_28_day_requesters),
+        all_time_commenters: json_for_api(@all_time_commenters),
+        last_28_day_commenters: json_for_api(@last_28_day_commenters)
+      }
+
+      respond_to do |format|
+        format.html
+        format.json { render json: api_data }
+      end
+    end
+
+    private
+
+    def json_for_api(data)
+      data.map { |u,c| { user: u.json_for_api, count: c } }
     end
   end
 end
