1dfa4cdfa0ef7d06c8dbea0b78add01139716451
Luke Bacon <luke@equivalentideas.com>
2016-10-27 13:48:05 +1100

    Extract variable

--- assets/javascripts/personal_message_toggler.js
+++ assets/javascripts/personal_message_toggler.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $switcherFieldGroup = $("#request_personal_switch");
   $formSubmitButton = $("#request_form input[type='submit']")[0];
+  personalRequestClass = "personal_request_switcher_focused";
 
   if ($switcherFieldGroup.length) {
     // If an error is showing they must have selected 'no'
@@ -8,17 +9,17 @@ $(document).ready(function() {
     if ($(".errorExplanation").length) {
       $("#request_personal_switch_no").prop("checked", true);
     } else {
-      $switcherFieldGroup.addClass("personal_request_switcher_focused");
+      $switcherFieldGroup.addClass(personalRequestClass);
       $formSubmitButton.disabled = true;
     }
 
     $("#request_personal_switch_no").click(function(e) {
-      $switcherFieldGroup.removeClass("personal_request_switcher_focused");
+      $switcherFieldGroup.removeClass(personalRequestClass);
       $formSubmitButton.disabled = false;
     });
 
     $("#request_personal_switch_yes").click(function(e) {
-      $switcherFieldGroup.addClass("personal_request_switcher_focused");
+      $switcherFieldGroup.addClass(personalRequestClass);
       $formSubmitButton.disabled = true;
     });
   }
