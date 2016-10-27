f279a11f415e82eab819c43e7e4c8e008c969998
Luke Bacon <luke@equivalentideas.com>
2016-10-27 13:22:25 +1100

    Handle case when the form loads with an error

--- assets/javascripts/personal_message_toggler.js
+++ assets/javascripts/personal_message_toggler.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
   $switcherFieldGroup = $("#request_personal_switch");
 
   if ($switcherFieldGroup.length) {
-    $switcherFieldGroup.addClass("personal_request_switcher_focused");
-
-    $("#request_personal_switch_no").click(function(e) {
-      $switcherFieldGroup.removeClass("personal_request_switcher_focused");
-    });
-
-    $("#request_personal_switch_yes").click(function(e) {
+    // If an error is showing they must have selected 'no'
+    // so set this and don't hide the form.
+    if ($(".errorExplanation").length) {
+      $("#request_personal_switch_no").prop("checked", true);
+    } else {
       $switcherFieldGroup.addClass("personal_request_switcher_focused");
-    });
+
+      $("#request_personal_switch_no").click(function(e) {
+        $switcherFieldGroup.removeClass("personal_request_switcher_focused");
+      });
+
+      $("#request_personal_switch_yes").click(function(e) {
+        $switcherFieldGroup.addClass("personal_request_switcher_focused");
+      });
+    }
   }
 });
