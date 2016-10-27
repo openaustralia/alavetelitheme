532d2df60b8c19bb811e1c1062b086e204023ca6
Luke Bacon <luke@equivalentideas.com>
2016-10-27 14:20:26 +1100

    Maintain state of the switch when people go back to edit after preview

--- assets/javascripts/personal_message_toggler.js
+++ assets/javascripts/personal_message_toggler.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
   personalRequestClass = "personal_request_switcher_focused";
 
   if ($switcherFieldGroup.length) {
-    // If an error is showing they must have selected 'no'
+    // If an error is showing or the subject has been filled then
+    // they must have selected 'no' already
     // so set this and don't hide the form.
-    if ($(".errorExplanation").length) {
+    if ($(".errorExplanation").length || $("#request_header_subject input")[0].value) {
       $("#request_personal_switch_no").prop("checked", true);
     } else {
       $switcherFieldGroup.addClass(personalRequestClass);
