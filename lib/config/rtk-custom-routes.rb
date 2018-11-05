782721ddcff97e9c7779bbc3fbee0799e64dde6a
Ben Fairless <ben@righttoknow.org.au>
2018-11-06 00:17:15 +0800

    Complaints Page

--- lib/config/rtk-custom-routes.rb
+++ lib/config/rtk-custom-routes.rb
@@ -3,5 +3,10 @@
 Alaveteli::Application.routes.draw do
     # brand new controller example
     match '/mycontroller' => 'general#mycontroller', :as => :mycontroller
+
+    get '/help/complaints'  => 'help#complaints',
+      :via => 'get',
+      :as => 'help_complaints'
+
     match '/people' => 'general#people', :as => :people
 end
--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -6,6 +6,7 @@
 #
 Rails.configuration.to_prepare do
   # Example adding an instance variable to the frontpage controller
+    def complaints; end
   GeneralController.class_eval do
     def mycontroller
       @say_something = "Greetings friend"
