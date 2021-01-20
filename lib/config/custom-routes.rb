ed93bfbff1ad741882f0eab18411df09bddfa3c5
Matthew Landauer <mlandauer@gmail.com>
2021-01-20 14:53:41 +1100

    Comment out custom routes that aren't being used

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -2,10 +2,10 @@
 
 Alaveteli::Application.routes.draw do
     # brand new controller example
-    match '/mycontroller' => 'general#mycontroller', :as => :mycontroller
+    # match '/mycontroller' => 'general#mycontroller', :as => :mycontroller
 
     # Additional help page example
-    match '/help/help_out' => 'help#help_out', :as => :help_out
+    # match '/help/help_out' => 'help#help_out', :as => :help_out
 
     match '/people' => 'general#people', :as => :people
 end
