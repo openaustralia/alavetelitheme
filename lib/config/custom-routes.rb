10b73457fb220b5d67937c85c6723075a899b280
Matthew Landauer <mlandauer@gmail.com>
2021-02-04 13:23:24 +1100

    people page is now part of standard statistics page. So, we can simplify.

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     # Additional help page example
     # get '/help/help_out' => 'help#help_out'
 
-    get '/people' => 'general#people'
+    # We used to have a dedicated people page which was made in this theme.
+    # Now that same information has been merged into the statistics page
+    # in the main project there is no need for our version. Just to be
+    # safe let's redirect the old url
+    get '/people' => redirect('statistics#people')
 end
--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -5,31 +5,4 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-  # Example adding an instance variable to the frontpage controller
-  GeneralController.class_eval do
-    def people
-      @all_time_requesters = User.all_time_requesters
-      @last_28_day_requesters = User.last_28_day_requesters
-      @all_time_commenters = User.all_time_commenters
-      @last_28_day_commenters = User.last_28_day_commenters
-
-      api_data = {
-        all_time_requesters: json_for_api(@all_time_requesters),
-        last_28_day_requesters: json_for_api(@last_28_day_requesters),
-        all_time_commenters: json_for_api(@all_time_commenters),
-        last_28_day_commenters: json_for_api(@last_28_day_commenters)
-      }
-
-      respond_to do |format|
-        format.html
-        format.json { render json: api_data }
-      end
-    end
-
-    private
-
-    def json_for_api(data)
-      data.map { |u,c| { user: u.json_for_api, count: c } }
-    end
-  end
 end
